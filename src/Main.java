4997c6bfa830fb1efe1aaf9e8dabe6e7aff6834c
Carolina <carolinahatakeyama2@gmail.com>
2025-11-28 21:10:36 -0300

    primeiro commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public class CalculadoraIMC {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            // Entrada de dados
+            System.out.print("Digite seu peso (kg): ");
+            double peso = sc.nextDouble();
+
+            System.out.print("Digite sua altura (m): ");
+            double altura = sc.nextDouble();
+
+            // Cálculo do IMC
+            double imc = peso / (altura * altura);
+
+            // Exibição do resultado
+            System.out.printf("Seu IMC é: %.2f%n", imc);
+            
+
         }
     }
+
 }
